d69817ddfc08864b9b101f755e85344fbc8a4ee8
Nikita Zhurawski <nikita.zhuravskiy@skywindgroup.com>
2023-07-02 17:09:15 +0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => {
 	return {
 		plugins: [react()],
-		base: mode === 'production' ? '/faraway-swapi-react/' : './',
+		base: '/',
+		// base: mode === 'production' ? '/faraway-swapi-react/' : './',
 	}
 })
